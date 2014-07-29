ede01594efcfe6abd38f4e242d71473d344c34ab
Ender Dai <xdai@uoregon.edu>
2014-07-28 17:59:58 -0700

    New config option to support basic pre-experiment copy/link
    Add config option "Experiments.copy"/"Experiments.link" to copy/link
    certain files into Experiments.rootdir before the experiment takes
    place. We may change this in the future if more sophisticated control
    is needed.

--- autoperf/experiment.py
+++ autoperf/experiment.py
@@ -1,4 +1,6 @@
 import os
+import shutil, errno
+import ConfigParser
 
 from .utils import config
 
@@ -19,15 +21,24 @@ class Experiment:
         self.tool_name     = config.get("%s.Tool"     % self.longname)
         self.analyses_name = config.get("%s.Analyses" % self.longname).split()
 
-        _module = __import__("platforms.%s" % self.platform_name, globals(), fromlist=["Platform"], level=1)
+        _module = __import__("platforms.%s" % self.platform_name,
+                             globals(),
+                             fromlist=["Platform"],
+                             level=1)
         self.platform = _module.Platform(self)
 
-        _module = __import__("tools.%s" % self.tool_name, globals(), fromlist=["Tool"], level=1)
+        _module = __import__("tools.%s" % self.tool_name,
+                             globals(),
+                             fromlist=["Tool"],
+                             level=1)
         self.tool = _module.Tool(self)
 
         self.analyses = dict()
         for analysis in self.analyses_name:
-            _module = __import__("analyses.%s" % analysis, globals(), fromlist=["Analysis"], level=1)
+            _module = __import__("analyses.%s" % analysis,
+                                 globals(),
+                                 fromlist=["Analysis"],
+                                 level=1)
             self.analyses[analysis] = _module.Analysis(self)
 
     def build(self):
@@ -38,6 +49,30 @@ class Experiment:
         if not os.path.isdir(cwd):
             os.makedirs(cwd)
         os.chdir(cwd)
+
+        # copy necessary files, if they are specified in config file
+        try:
+            for item in config.get("%s.copy" % self.longname).split():
+                print "Copying %s ..." % item
+                item = os.path.expanduser(item)
+                try:
+                    shutil.copytree(item, os.path.basename(item), True)
+                except OSError as e:
+                    if e.errno == errno.ENOTDIR:
+                        shutil.copy(item, os.path.basename(item))
+                    else:
+                        raise
+        except ConfigParser.Error:
+            pass
+
+        # link necessary files, if they are specified in config file
+        try:
+            for item in config.get("%s.link" % self.longname).split():
+                print "Linking %s ..." % item
+                item = os.path.expanduser(item)
+                os.symlink(item, os.path.basename(item))
+        except ConfigParser.Error:
+            pass
 
         self.platform.setup()
         self.tool.setup()
